49269de5b8087f436955555df18754266d8f198c
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-05-22 16:52:24 +0530

    Update and rename loop.py to loop.py

--- ChinnaXmusic/plugins/admins/loop.py
+++ ChinnaXmusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ShrutiMusic import app
-from ShrutiMusic.utils.database import get_loop, set_loop
-from ShrutiMusic.utils.decorators import AdminRightsCheck
-from ShrutiMusic.utils.inline import close_markup
+from ChinnaXmusic import app
+from ChinnaXmusic.utils.database import get_loop, set_loop
+from ChinnaXmusic.utils.decorators import AdminRightsCheck
+from ChinnaXmusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
