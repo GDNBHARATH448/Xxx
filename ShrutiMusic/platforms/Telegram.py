c744db69dd924f453f2432b0081ef7991c2792bf
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-05-22 11:49:07 +0530

    Update Telegram.py

--- ShrutiMusic/platforms/Telegram.py
+++ ShrutiMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from ShrutiMusic import app
-from ShrutiMusic.utils.formatters import (
+from ChinnaXmusic import app
+from ChinnaXmusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
