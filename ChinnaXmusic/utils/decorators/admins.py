5590df663bb85bd84707afae67b034f17bbe6642
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-05-22 12:08:22 +0530

    Update and rename admins.py to admins.py

--- ChinnaXmusic/utils/decorators/admins.py
+++ ChinnaXmusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from ShrutiMusic import app
-from ShrutiMusic.misc import SUDOERS, db
-from ShrutiMusic.utils.database import (
+from ChinnaXmusic import app
+from ChinnaXmusic.misc import SUDOERS, db
+from ChinnaXmusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
