c6c790fa36c729f4e9b0a2d273041c4567e4b7dd
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-05-22 13:49:51 +0530

    Update and rename help.py to help.py

--- ChinnaXmusic/utils/inline/help.py
+++ ChinnaXmusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from ShrutiMusic import app
+from ChinnaXmusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
